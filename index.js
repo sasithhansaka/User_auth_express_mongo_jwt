d265b2ab665af06138b61580a7da7d5f43b72f13
sasithhansaka <sasithhansaka285@gmail.com>
2024-12-29 11:51:11 +0530

    database connectes

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express";
+import db_connection from "./Config/db_connection.js";
 
+import dotenv from "dotenv";
 
 const app = express();
 
 const port = 5002;
 
 app.use(express.json());
+dotenv.config();
+
+db_connection();
 
 console.log("I am pro in express");
 app.listen(port, () => {
